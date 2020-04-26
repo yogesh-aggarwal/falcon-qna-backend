c73816852b3eac0f9179b2857f9809bd652ddca4
Yogesh Aggarwal <developeryogeshgit@gmail.com>
2020-04-26 11:32:23 +0530

    Added server configurations & apollo integration

---
+++ app.ts
@@ -0,0 +1,23 @@
+const { gql, ApolloServer } = require("apollo-server");
+const { importSchema } = require("graphql-import");
+
+const mongoose = require("mongoose");
+
+const typeDefs = gql(importSchema("./graphql/schema.gql"));
+const resolvers = require("./resolver");
+
+require("dotenv").config(); // Configuring env variables
+
+mongoose
+  .connect(process.env.DBURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    const server = new ApolloServer({ typeDefs, resolvers });
+    server.listen({ port: process.env.PORT }).then((info: { port: number }) => {
+      console.log(
+        `🚀 Server is up and running at http://localhost:${info.port}`
+      );
+    });
+  });
